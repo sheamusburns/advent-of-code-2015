8aeebea3dd2158b8b22ebc9a00dd844417caa6cc
Sheamus <sheamusburns@gmail.com>
2015-12-28 02:24:55 -0500

    part 1 day7 complete. I am in hell. I think at one point I had 300 lines of code.

--- day7.js
+++ day7.js
@@ -2,41 +2,52 @@ var fs = require('fs');
 var csv = require('csv');
 var input = csv.stringify(fs.readFileSync('./day7input.js', 'utf8')).options.split('\n');
 
-console.log(input);
-var wires = {};
+var values = {};
+var count = 0;
 var reWireNames = /[a-z]+/g;
-var reOperator = /[A-z]+/g;
+var reOperator = /([A-Z])\w+/g;
 var reDigit = /\d+/g;
 var reInit = /^\d+\s->\s[a-z]+/g;
 var reKey = /[a-z]+$/g;
 var reFormula = /^.*(?=\s->)/g;
-/*var operators = {
-	AND: &,
-	OR: |,
-	NOT: ~,
-	LSHIFT: <<,
-	RSHIFT: >>
-}*/
-input.forEach(function(item, ind, array){
-	wires[item.match(reKey)] = item.match(reFormula)[0];
-	// var tempArr = item.match(reWireNames);
-	// tempArr.forEach(function(match){
-	// 	if (!wires[match]) {
-	// 		wires[match] = null;
-	// 	}
-	// });
-	// if (item.match(reInit)) {
-	// 	wires[item.match(reWireNames)] = Number(item.match(reDigit));
-	// 	array[ind] = '';
-	// }
-	//console.log(item.match(/->.$'/g));
-});
-function lookup(wireName){
-	if (typeof wires[wireName] === Number) {
+var operators = {
+	AND: '&',
+	OR: '|',
+	NOT: '~',
+	LSHIFT: '<<',
+	RSHIFT: '>>'
+};
 
+//prep values object for circIt()
+input.forEach(function(item, ind, array){
+	if (item.match(reFormula)[0] > -1) {
+		values[item.match(reKey)] = Number(item.match(reFormula)[0]);
+	} else if (item.match(reOperator)){
+		array[ind] = array[ind].replace(reOperator, operators[item.match(reOperator)[0]]);
+		values[item.match(reKey)[0]] = array[ind].match(reFormula)[0];	
 	} else {
-		lookup()
+		values[item.match(reKey)[0]] = array[ind].match(reFormula)[0];
+	}	
+});
+
+circIt();
+
+function circIt(){
+	count = 0
+	for (obj in values) {
+		if (typeof values[obj] === 'string') {
+			var wires = values[obj].match(reWireNames);
+			(wires && wires.length)
+			? function(){
+				count++
+				for (var i = 0; i < wires.length; i++){
+					values[obj] = (typeof values[wires[i]] === 'number') ? 
+					values[obj].replace(wires[i], values[wires[i]]) :
+					values[obj]
+				}
+			}()
+			: values[obj] = eval(values[obj]);
+		}
 	}
+	(count > 0) ? circIt() : console.log("a: %s", values.a)
 }
-console.log(wires.a);
-// console.log(input);
