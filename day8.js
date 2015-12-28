237177609f47a3ed87de22d2aad2472f7aa35b03
Sheamus <sheamusburns@gmail.com>
2015-12-28 11:52:12 -0500

    refactored

--- day8.js
+++ day8.js
@@ -1,19 +1,17 @@
 var fs = require('fs');
 var csv = require('csv');
 var input = csv.stringify(fs.readFileSync('./day8Input.js', 'utf8')).options.split('\n');
-var charCount = 0;
-var strCount = 0;
-var newEncodeCount = 0;
-var diff = 0;
 
-day8(1);
-day8(2);
 
-function day8(part){
-	charCount = 0;
-	strCount = 0;
-	newEncodeCount = 0;
-	diff = 0;
+day8([1, 2]);
+
+
+function day8(parts){
+	var charCount = 0;
+	var strCount = 0;
+	var newEncodeCount = 0;
+	var diff = 0;
+	var part;
 
 	input.forEach(function(str){
 		charCount += str.length;
@@ -21,7 +19,10 @@ function day8(part){
 		newEncodeCount += JSON.stringify(str).length;
 	});
 
-	diff = (part === 1) ? (charCount - strCount)
-	: (part === 2) ? (newEncodeCount - charCount) : diff;
-	console.log('the answer for part %s: %s', part, diff);
+	for (i in parts) {
+		part = parts[i];
+		diff = (part === 1) ? (charCount - strCount)
+		: (part === 2) ? (newEncodeCount - charCount) : diff;
+		console.log('the answer for part %s: %s', part, diff);
+	}
 }
