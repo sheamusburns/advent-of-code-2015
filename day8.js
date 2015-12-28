6df37804687c2eebc6b0db2ac1cd8a3673c363b2
Sheamus <sheamusburns@gmail.com>
2015-12-28 11:43:51 -0500

    finished day 8

---
+++ day8.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+var csv = require('csv');
+var input = csv.stringify(fs.readFileSync('./day8Input.js', 'utf8')).options.split('\n');
+var charCount = 0;
+var strCount = 0;
+var newEncodeCount = 0;
+var diff = 0;
+
+day8(1);
+day8(2);
+
+function day8(part){
+	charCount = 0;
+	strCount = 0;
+	newEncodeCount = 0;
+	diff = 0;
+
+	input.forEach(function(str){
+		charCount += str.length;
+		strCount += eval(str).length;
+		newEncodeCount += JSON.stringify(str).length;
+	});
+
+	diff = (part === 1) ? (charCount - strCount)
+	: (part === 2) ? (newEncodeCount - charCount) : diff;
+	console.log('the answer for part %s: %s', part, diff);
+}
