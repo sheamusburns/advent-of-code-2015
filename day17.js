27a1a64f10ff23fde09662540fb3e1bed1157e4e
Sheamus <sheamusburns@gmail.com>
2015-12-31 22:58:15 -0500

    finally finished.. this was tough -- what with the generating combinations thing -- had to steal it.

--- day17.js
+++ day17.js
@@ -1,33 +1,99 @@
 var fs = require('fs');
 var csv = require('csv');
 var input = csv.stringify(fs.readFileSync('./day17Input.js', 'utf8')).options.split('\n');
-var containers = [];
 var totEggnog = 150;
+var containers = [];
+var combos = [];
+var matches = [];
+var minMatch = [];
+
 input.forEach(function(line, ind, arr){
 	containers.push(Number(line));
 });
-console.log(containers);
 
-var conArr = [];
+combos = combinations(containers);
+var count = 0
+for (item in combos) {
+	count = 0;
+	combo = combos[item];
+	for (i in combo) {
+		count += combo[i];
+	}
+	if (count === totEggnog){
+		matches.push(combos[item]);
+	}
+};
 
-
-for (var a = 0; a < containers.length; a++){
-	tempArr = [];
-	tempArr.push(containers[a]);
-	for (con in containers) {
-		permuteCon();
-		
+var minCount = 0;
+for (i in matches){
+	var match = matches[i];
+	if (minCount === 0) {
+		minCount = match.length;
+	} else if (minCount > match.length) {
+		minCount = match.length;
 	}
 }
-console.log(conArr);
+for (i in matches){
+	if (matches[i].length === minCount){
+		minMatch.push(matches[i]);
+	}
+}
 
-function permuteCon(){
-	if (containers[con] !== containers[a]){
-		tempCount = 0;
-		if (containers[con] + containers[a] < 100){
-			tempArr.push(containers[a]);
-			con++
-			permuteCon()
+console.log('matches totalling exactly %s: %s', totEggnog, matches.length);
+console.log('mininum containers:', minCount);
+console.log('number of combos of minimum containers:', minMatch.length);
+
+
+/*
+// After several attempts at writing a function to return all the possible
+// combinations in the set, I ended up finding this useful pair of functions
+// github gist.
+//
+// combinations.js
+// gist.github.com/axelpale/3118596
+// by @axelpale
+*/
+
+function k_combinations(set, k) {
+	var i, j, combs, head, tailcombs;
+	
+	if (k > set.length || k <= 0) {
+		return [];
+	}
+	
+	if (k == set.length) {
+		return [set];
+	}
+	
+	if (k == 1) {
+		combs = [];
+		for (i = 0; i < set.length; i++) {
+			combs.push([set[i]]);
+		}
+		return combs;
+	}
+	
+	combs = [];
+	for (i = 0; i < set.length - k + 1; i++) {
+		head = set.slice(i, i+1);
+		tailcombs = k_combinations(set.slice(i + 1), k - 1);
+		for (j = 0; j < tailcombs.length; j++) {
+			combs.push(head.concat(tailcombs[j]));
 		}
 	}
-}	
+	return combs;
+}
+
+function combinations(set) {
+	var k, i, combs, k_combs;
+	combs = [];
+	
+	// Calculate all non-empty k-combinations
+	for (k = 1; k <= set.length; k++) {
+		k_combs = k_combinations(set, k);
+		for (i = 0; i < k_combs.length; i++) {
+			combs.push(k_combs[i]);
+		}
+	}
+	return combs;
+}
