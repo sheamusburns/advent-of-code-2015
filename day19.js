4816489768a81246db9faa15f26ce1ec239c965a
Sheamus <sheamusburns@gmail.com>
2016-01-03 20:56:18 -0500

    finished day 19 - part 2 was deceptively hard -- made you want to think the solution was much more complex than it needed to be

--- day19.js
+++ day19.js
@@ -3,13 +3,16 @@ var csv = require('csv');
 var calibrator = 'CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF';
 var input = csv.stringify(fs.readFileSync('./day19Input.js', 'utf8')).options.split('\n');
 var keys = [];
+var mutations = [];
+var mutationRules = {};
 var rules = {};
-var combinations = [];
+var key, val;
 
 input.forEach(function(line, ind, arr){
 	key = line.match(/\w+(?= \=>)/g)[0];
-	if (keys.indexOf(key) < 0) keys.push(key);
 	val = line.match(/\w+$/g)[0];
+	if (keys.indexOf(key) < 0) keys.push(key)
+	if (mutations.indexOf(val) < 0)	mutations.push(val)
 	if (!rules[key]) rules[key] = {
 		mutatesTo: [],
 		indices: [],
@@ -17,60 +20,38 @@ input.forEach(function(line, ind, arr){
 	};
 	rules[key].mutatesTo.push(val);
 });
-// console.log(rules);
-// console.log(keys);
 
-
-// genCombos(calibrator);
-var match = false;
-var checkStepNum = 0;
-genCombos(calibrator).length; //create first step
-
-
-
-function run(){
-	checkLastStep(combinations[checkStepNum]);
-	if (!match) {
-		createStep(combinations[checkStepNum]);
-		checkStepNum++;
-		run()
-	}
-}
-function checkLastStep(lastStep){
-	for (comb in lastStep){
-		var s = combinations[comb];
-		for (i in s){
-			if (s[i].join('') === calibrator) {
-				console.log('found it');
-				match = true;
-				return checkStepNum;
-			}
+for(mu in mutations){
+	mutation = mutations[mu];
+	for (i in rules) {
+		obj = rules[i];
+		if (obj.mutatesTo.indexOf(mutation) > -1) {
+			if (!mutationRules[mutation]) mutationRules[mutation] = [];
+			mutationRules[mutation].push(i);
 		}
 	}
 }
 
+reverseEngineer(calibrator);
+genCombos(calibrator);
 
-// console.log(combinations.length);
-// console.log(combinations[1])
 
-var count = 0; 
-function createStep(lastStep){
-	for (var h = 0; h < lastStep.length; h++){
-		// console.log(lastStep[h]);
-		var initState = lastStep[h][0];
-		var stepArr = [];
-		// for (item in combinations){
-		// 	stepArr = [];
-		// 	var step = combinations[item];
-		// 	var stepNum = item+1;
-		// 	for (var i = 0; i < step.length; i++){
-		// 		stepArr.push(genCombos(step[i].join('')));
-		// 	}		
-		stepArr.push(genCombos(initState));	
+function reverseEngineer(mol){
+count = 0;
+var target = calibrator;
+var tempStr = '';
+while (target !== 'e'){
+	for (i in mutations) {
+		if (target.indexOf(mutations[i]) > -1){
+			val = target.indexOf(mutations[i]);
+			code = mutationRules[mutations[i]];
+			target = target.slice(0, val) + code + target.slice(val + mutations[i].length)
+			count++
+		}
 	}
-	combinations.push(stepArr);
 }
-	
+console.log('reverse engineer from molecule to \'e\':', count);
+}
 
 function genCombos(mol) {
 	var tempCombos = [];
@@ -78,7 +59,6 @@ function genCombos(mol) {
 		regex = new RegExp(key, 'g');
 		while(match = regex.exec(mol)) {
 			rules[key].indices.push(match.index);
-			// console.log(match[0], match.index);
 		}
 		for (item in rules[key].mutatesTo){
 			code = rules[key].mutatesTo[item];
@@ -89,13 +69,9 @@ function genCombos(mol) {
 				if (tempCombos.indexOf(tempStr) < 0) {
 					tempCombos.push(tempStr);
 				}
-				// console.log(tempStr, key);
-				rules[key].combos.push(calibrator)
+				rules[key].combos.push(tempStr)
 			})
 		}
-		// (matchArr) ? console.log(matchArr.length) : console.log('no matches');
-		// console.log(matchArr);
 	});
-	console.log("number of combinations:", tempCombos.length);
-	return tempCombos;
+	console.log("calibration", tempCombos.length);
 }
