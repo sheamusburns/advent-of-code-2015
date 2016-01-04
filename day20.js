611f2ae4b51927edc38c5c25e89fcba26120c63c
Sheamus <sheamusburns@gmail.com>
2016-01-04 15:29:25 -0500

    finished part 2

--- day20.js
+++ day20.js
@@ -2,21 +2,25 @@ var input = 34000000;
 var houses = [];
 
 var arr = [];
-var count = 0;
+count = 0;
 
 
 console.time('objOrArr'); //testing object vs array speed
 for (var elf = 1; elf < 958309; elf ++) {
+	elfHouseCount = 0;
 	if (count % 1000 === 0) {
 		process.stdout.write('.')
 	}
 	for (var house = elf; house <= 958309; house += elf){
-		// console.log(house, elf)
-		if (!houses[house]) {
+		if (elfHouseCount === 50 ){ // Part 2
+			break;
+		} else if (!houses[house]) {
 			houses[house] = 0;
 		}
 		if (house % elf === 0){
-			houses[house] += 10*elf;
+			//houses[house] += 10*elf; // Part 1
+			houses[house] += 11*elf;  // Part 2
+			elfHouseCount ++;
 		}
 		if (houses[house] >= input) {
 			console.timeEnd('objOrArr');
@@ -29,32 +33,3 @@ for (var elf = 1; elf < 958309; elf ++) {
 }
 console.timeEnd('objOrArr');
 console.log(houses);
-
-
-
-
-// this does nothing
-
-// equation(input);
-
-
-// function equation(input) {
-// 	var sum = 0;
-// 	var count = 0;
-// 	for (var elf = 1; elf < 1000000; elf ++) {
-// 		if (input % elf === 0) {
-// 			input = input - elf*10;
-// 			count += elf*10;
-// 			sum += elf;
-// 			console.log(input, elf, count)
-			
-			
-// 		}
-// 		if (input === 0) {
-// 			console.log(count);
-// 			return;
-// 		}
-// 	}
-// }
-
-
